27c904d795b4c286a4a9927f98126271e643f524
Ammanuel Roberts <ammanuelroberts@ammanuels-air>
2016-11-24 21:44:41 -0500

    updated

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,5 +1,67 @@
 //: Playground - noun: a place where people can play
+// Basics, Control Flow, Functions
+
 
 import UIKit
 
 var str = "Hello, playground"
+var teammateHeights: [String: Int] = [
+    "Joe Smith": 42,
+    "Jill Tanner": 36,
+    "Bill Bon": 43,
+    "Eva Gordon": 45,
+    "Matt Gill": 40,
+    "Kimmy Stein": 41,
+    "Sammy Adams": 45,
+    "Karl Saygan": 42,
+    "Suzane Greenberg": 44,
+    "Sal Dali": 41,
+    "Joe Kavalier": 39,
+    "Ben Finkelstein": 44,
+    "Diego Soto": 41,
+    "Chloe Alaska": 47,
+    "Arnold Willis": 43,
+    "Phillip Helm": 44,
+    "Les Clay": 42,
+    "Herschel Krustofski": 45
+]
+var teammateHeights: [String: Bool] = [
+    "Joe Smith": true,
+    "Jill Tanner": true,
+    "Bill Bon": true,
+    "Eva Gordon": false,
+    "Matt Gill": false,
+    "Kimmy Stein": false,
+    "Sammy Adams": false,
+    "Karl Saygan": true,
+    "Suzane Greenberg": true,
+    "Sal Dali": false,
+    "Joe Kavalier": false,
+    "Ben Finkelstein": false,
+    "Diego Soto": true,
+    "Chloe Alaska": false,
+    "Arnold Willis": false,
+    "Phillip Helm": true,
+    "Les Clay": true,
+    "Herschel Krustofski": true
+]
+var teammateGuardianNames: [String: Int] = [
+    "Joe Smith": "Jim and",
+    "Jill Tanner": 36,
+    "Bill Bon": 43,
+    "Eva Gordon": 45,
+    "Matt Gill": 40,
+    "Kimmy Stein": 41,
+    "Sammy Adams": 45,
+    "Karl Saygan": 42,
+    "Suzane Greenberg": 44,
+    "Sal Dali": 41,
+    "Joe Kavalier": 39,
+    "Ben Finkelstein": 44,
+    "Diego Soto": 41,
+    "Chloe Alaska": 47,
+    "Arnold Willis": 43,
+    "Phillip Helm": 44,
+    "Les Clay": 42,
+    "Herschel Krustofski": 45
+]
